619f1174120d4602f610e8ea7618787d6dd8850a
Ryan1729 <Ryan1729@gmail.com>
2021-09-10 01:06:00 -0600

    copy a bunch of stuff from sundered-tiles, and get it compiling
    See https://github.com/Ryan1729/sundered-tiles

---
+++ find-the-randomness/src/main.rs
@@ -0,0 +1,144 @@
+//#![deny(unused)]
+
+extern crate alloc;
+use alloc::vec::Vec;
+
+struct Storage<A>(Vec<A>);
+
+impl <A> game::ClearableStorage<A> for Storage<A> {
+    fn clear(&mut self) {
+        self.0.clear();
+    }
+
+    fn push(&mut self, a: A) {
+        self.0.push(a);
+    }
+}
+
+use raylib::prelude::{
+    *,
+    KeyboardKey::*,
+    core::{
+        logging
+    }
+};
+
+fn draw_wh(rl: &RaylibHandle) -> game::DrawWH {
+    game::DrawWH {
+        w: rl.get_screen_width() as game::DrawW,
+        h: rl.get_screen_height() as game::DrawH,
+    }
+}
+
+fn main() {
+    let (mut rl, thread) = raylib::init()
+    .size(0, 0)
+    .resizable()
+    .title("Find the Randomness")
+    .build();
+
+    if cfg!(debug_assertions) {
+        logging::set_trace_log_exit(TraceLogType::LOG_WARNING);
+    }
+
+    rl.set_target_fps(60);
+    rl.toggle_fullscreen();
+
+    let seed: u128 = {
+        use std::time::SystemTime;
+
+        let duration = match 
+            SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+        {
+            Ok(d) => d,
+            Err(err) => err.duration(),
+        };
+
+        duration.as_nanos()
+    };
+    println!("{}", seed);
+
+    let mut state = game::State::from_seed(seed.to_le_bytes());
+    let mut commands = Storage(Vec::with_capacity(1024));
+
+    // generate the commands for the first frame
+    game::update(&mut state, &mut commands, 0, draw_wh(&rl));
+
+    const BACKGROUND: Color = Color{ r: 0x22, g: 0x22, b: 0x22, a: 255 };
+    const WHITE: Color = Color{ r: 0xee, g: 0xee, b: 0xee, a: 255 };
+
+    while !rl.window_should_close() {
+        if rl.is_key_pressed(KEY_F11) {
+            rl.toggle_fullscreen();
+        }
+
+        let mut input_flags = 0;
+
+        if rl.is_key_pressed(KEY_SPACE) || rl.is_key_pressed(KEY_ENTER) {
+            input_flags |= game::INPUT_INTERACT_PRESSED;
+        }
+
+        if rl.is_key_down(KEY_UP) || rl.is_key_down(KEY_W) {
+            input_flags |= game::INPUT_UP_DOWN;
+        }
+
+        if rl.is_key_down(KEY_DOWN) || rl.is_key_down(KEY_S) {
+            input_flags |= game::INPUT_DOWN_DOWN;
+        }
+
+        if rl.is_key_down(KEY_LEFT) || rl.is_key_down(KEY_A) {
+            input_flags |= game::INPUT_LEFT_DOWN;
+        }
+
+        if rl.is_key_down(KEY_RIGHT) || rl.is_key_down(KEY_D) {
+            input_flags |= game::INPUT_RIGHT_DOWN;
+        }
+
+        if rl.is_key_pressed(KEY_UP) || rl.is_key_pressed(KEY_W) {
+            input_flags |= game::INPUT_UP_PRESSED;
+        }
+        
+        if rl.is_key_pressed(KEY_DOWN) || rl.is_key_pressed(KEY_S) {
+            input_flags |= game::INPUT_DOWN_PRESSED;
+        }
+
+        if rl.is_key_pressed(KEY_LEFT) || rl.is_key_pressed(KEY_A) {
+            input_flags |= game::INPUT_LEFT_PRESSED;
+        }
+
+        if rl.is_key_pressed(KEY_RIGHT) || rl.is_key_pressed(KEY_D) {
+            input_flags |= game::INPUT_RIGHT_PRESSED;
+        }
+
+        game::update(
+            &mut state,
+            &mut commands,
+            input_flags,
+            draw_wh(&rl)
+        );
+
+        let screen_render_rect = Rectangle {
+            x: 0.,
+            y: 0.,
+            width: rl.get_screen_width() as _,
+            height: rl.get_screen_height() as _
+        };
+
+        let sizes = game::sizes(&state);
+
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(BACKGROUND);
+
+        // the -1 and +2 business makes the border lie just outside the actual
+        // play area
+        d.draw_rectangle_lines(
+            sizes.play_xywh.x as i32 - 1,
+            sizes.play_xywh.y as i32 - 1,
+            sizes.play_xywh.w as i32 + 2,
+            sizes.play_xywh.h as i32 + 2,
+            WHITE
+        );
+    }
+}
---
+++ game/src/game.rs
@@ -0,0 +1,59 @@
+pub trait ClearableStorage<A> {
+    fn clear(&mut self);
+
+    fn push(&mut self, a: A);
+}
+
+pub type Seed = [u8; 16];
+
+pub mod draw;
+
+pub use draw::{
+    DrawLength,
+    DrawX,
+    DrawY, 
+    DrawXY,
+    DrawW,
+    DrawH,
+    DrawWH,
+};
+
+#[derive(Debug, Default)]
+pub struct State {
+    sizes: draw::Sizes,
+}
+
+impl State {
+    pub fn from_seed(seed: Seed) -> Self {
+        Self {
+            ..<_>::default()
+        }
+    }
+}
+
+pub fn sizes(state: &State) -> draw::Sizes {
+    state.sizes.clone()
+}
+
+pub type InputFlags = u16;
+
+pub const INPUT_UP_PRESSED: InputFlags              = 0b0000_0000_0000_0001;
+pub const INPUT_DOWN_PRESSED: InputFlags            = 0b0000_0000_0000_0010;
+pub const INPUT_LEFT_PRESSED: InputFlags            = 0b0000_0000_0000_0100;
+pub const INPUT_RIGHT_PRESSED: InputFlags           = 0b0000_0000_0000_1000;
+
+pub const INPUT_UP_DOWN: InputFlags                 = 0b0000_0000_0001_0000;
+pub const INPUT_DOWN_DOWN: InputFlags               = 0b0000_0000_0010_0000;
+pub const INPUT_LEFT_DOWN: InputFlags               = 0b0000_0000_0100_0000;
+pub const INPUT_RIGHT_DOWN: InputFlags              = 0b0000_0000_1000_0000;
+
+pub const INPUT_INTERACT_PRESSED: InputFlags        = 0b0000_0001_0000_0000;
+
+pub fn update(
+    state: &mut State,
+    commands: &mut dyn ClearableStorage<draw::Command>,
+    input_flags: InputFlags,
+    draw_wh: DrawWH,
+) {
+
+}
